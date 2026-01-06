01f421f9f28c8f12a2899b510964b7507b055c17
Tempt23 <slydahlh@gmail.com>
2026-01-06 17:57:25 +0100

    Laget prosjekt og git repostitory

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hei from './components/Hei.jsx'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hei.jsx
@@ -0,0 +1,6 @@
+function Hei() {
+  return <h1>Hei verden!</h1>
+}
+
+
+export default Hei;
